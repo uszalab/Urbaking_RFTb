86e8215e6651e09913fbe020e41f5fc935eaeab8
Goldman István <goldmanistvan@gmail.com>
2015-12-28 17:27:05 +0100

    Befejeztem a felhasznalokezelo.php fáljt. A felvételt kivettem mert az úgyis regisztráció által lesz megoldva.Igy maradt a kilistázáa és törlés funkció.

--- web/felhasznalokezelo.php
+++ web/felhasznalokezelo.php
@@ -1,7 +1,7 @@
 <?php
 function kapcsolat(){
-    // még nincs meg az adatbázis neve
-     $kapcsolat = new PDO('mysql:dbname=;host=localhost','root','');
+    // phpmyadminban rft néven kell megcsinálni az adatbázist
+     $kapcsolat = new PDO('mysql:dbname=rft;host=localhost','root','');
      $kapcsolat->exec("set names utf8");
      return $kapcsolat;
      
@@ -11,13 +11,6 @@ function kapcsolatLezar($kapcsolat){
     $kapcsolat = null;
 }
 
-function ParameterekElokeszit($parameterek){
-    $sqlParameterek = [];
-    foreach($parameterek as $kulcs => $ertek){
-        $sqlParameterek[':'.$kulcs] = $ertek;
-    }
-    return $sqlParameterek;
-}
 function FelhasznaloLekerdez($id){
     $kapcsolat = kapcsolat();
     $lekerdezes = $kapcsolat->prepare("SELECT id, nev, email, telefon
@@ -36,17 +29,6 @@ function FelhasznaloTorol($felhasznaloId){
     $lekerdezes = $kapcsolat->prepare("DELETE FROM felhasznalo WHERE id = :id");
     $lekerdezes->bindParam(':id',$felhasznaloId);
     $sikeres = $lekerdezes->execute();
-    kapcsolatLezar($kapcsolat);
-    return $sikeres;
-}
-
-function FelhasznaloRogzit($parameterek){
-    $kapcsolat = kapcsolat();
-    $lekerdezes = $kapcsolat->prepare("INSERT INTO felhasznalo (nev, jelszo, email, telefon)
-                                          VALUES(:nev, :email, :telefon)");
-   
-    $sqlParametek = ParameterekElokeszit($parameterek);
-    $sikeres = $lekerdezes->execute($sqlParametek);
     kapcsolatLezar($kapcsolat);
     return $sikeres;
 }
@@ -76,39 +58,7 @@ function OsszFelhasznaloTorol(){
         }
     }
     
-    if(array_key_exists('ujFelhasznalo', $_POST)){
-        $parameterek = [
-            'nev'   =>  $_POST['nev'],
-            'jelszo'  => $_POST['jelszo'],
-            'email'   => $_POST['email'],
-            'telefon'     => $_POST['telefon']            
-        ];
-        
-        if(FelhasznaloRogzit($parameterek)){
-            echo "Sikeres felhasznaló rögzítés!";
-        }
-        else{
-            echo "Sikertelen rögzítés!";
-        }
-    }
-    
-    
 ?>
-<form action="" method="POST">
-    <label for="nev">Név:</label>
-    <input type="text" name="nev" value=""/><br/>
-    
-    <label for="szulhely">Jelszó</label>
-    <input type="password" name="jelszo" value=""/><br/>
-    
-    <label for="email">E-mail:</label>
-    <input type="text" name="email" value=""/><br/>
-    
-    <label for="email">Telefon:</label>
-    <input type="tel" name="telefon" value=""/><br/>
-    <button type="submit" name="ujTag">Mentés</button>
-    <button type="reset" name="reset">Alaphelyzetbe állít</button>
-</form>
 
 <form action="" method="POST">
     <button type="submit" name="ossztorol">Összes rekord törlése</button>
@@ -134,12 +84,10 @@ function OsszFelhasznaloTorol(){
             <td>E-mail</td>
             <td>Telefon</td>
             <td>Felhasználó törlése</td>
-            <td>Felhasználó módosítása</td>
         </tr>
     </thead>
     <tbody>
         <?php
-//        még nem pontosak a oszlopnevek
             $felhasznalok = FelhasznalokLekerdez();
             $felhasznalokDb = count($felhasznalok);
             for($i = 0; $i < $felhasznalokDb;$i++){
@@ -148,8 +96,7 @@ function OsszFelhasznaloTorol(){
                         <td>'.$felhasznalok[$i]['nev'].'</td>
                         <td>'.$felhasznalok[$i]['telefon'].'</td>
                         <td>'.$felhasznalok[$i]['email'].'</td>
-                        <td><button type="submit" name="torol" value="'.$felhasznalok[$i]['id'].'">Törlés</button></td>
-                        <td><button type="submit" name="modosit" value="'.$felhasznalok[$i]['id'].'">Modosít</button></td>    
+                        <td><button type="submit" name="torol" value="'.$felhasznalok[$i]['id'].'">Törlés</button></td>   
                      </tr>';
             }
         
